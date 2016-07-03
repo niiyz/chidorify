c9b1ed47ddc342ebbd475153ac65021d5dafbae2
niiyz <yoshida@niiyz.com>
2016-07-03 23:59:39 +0900

    Change directory name.

--- src/examples/example.js
+++ src/examples/example.js
@@ -4,4 +4,4 @@ import fs from 'fs'
 import data from '../../data/weaver4-1.json';
 import { weave } from '../weaver';
 
-fs.writeFile('index.html', weave(data, 800, 800))
+fs.writeFile('examples/index.html', weave(data, 800, 800))
