0b98cdb36381bdac759d080fad6b79fa36731d73
niiyz <yoshida@niiyz.com>
2016-07-03 22:31:14 +0900

    Move data directory

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import fs from 'fs'
-import data from '../weaver.json';
+import data from '../data/weaver4-1.json';
 import { weave } from './weaver';
 
 fs.writeFile('index.html', weave(data, 800, 800))
