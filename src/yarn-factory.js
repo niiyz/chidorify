d964b9e9659e006b911a3e4a15aae3dff517faaa
niiyz <yoshida@niiyz.com>
2016-07-09 09:59:10 +0900

    Add yarn factory

---
+++ src/yarn-factory.js
@@ -0,0 +1,13 @@
+import { Yarn } from './yarn.js'
+
+export class YarnFactory {
+
+  static nomal(color) {
+    return new Yarn(color);
+  }
+
+  static wide(color) {
+    return new Yarn(color, 2);
+  }
+
+}
