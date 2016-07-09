c86e3b6e75bf1ddb55a92934c03d84eaf601e18a
niiyz <yoshida@niiyz.com>
2016-07-09 09:57:59 +0900

    Yern -> Yarn

--- src/yarn.js
+++ src/yarn.js
@@ -1,8 +1,11 @@
-export class Yean {
+export class Yarn {
 
   constructor(color = '#000', thickness = 1) {
     this.color = color;
     this.thickness = thickness;
   }
-  
+
+  wide() {
+    this.thickness = 2;
+  }
 }
